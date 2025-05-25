9ce0d485c4484409e5a2439f8b3ab346a2b9d195
PeterMeter536 <dg@optimatik.ch>
2025-05-25 19:15:05 +0200

    Delete Moodle/Moodle_NEW/moodledata directory

--- Moodle/Moodle_NEW/moodledata/cache/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-// ********** This file is generated DO NOT EDIT **********
-$CFG->siteidentifier = '7el5H7Ae2MWqeu9LYdFnOJ8nLStZVRBflocalhost';
-$CFG->bootstraphash = 'e85b2d27a64f5c832054bc0246c1f24f';
-// Only if the file is not stale and has not been defined.
-if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
-    define('SYSCONTEXTID', 1);
-}
